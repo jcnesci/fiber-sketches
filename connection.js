4978bf37bef40521d47cbc319acd33ea30aaba72
Sam Kronick <sam.kronick@gmail.com>
2013-09-09 21:08:18 -0500

    Initial commit

---
+++ connection.js
@@ -0,0 +1,51 @@
+// Connection prototype
+// --------------------
+function Connection(a, b, type, strength) {
+	this.a = a;	// Device A
+	this.b = b; // Device B
+	this.type = type;
+	this.strength = strength;
+
+	this.a.connections.push(this)
+	this.b.connections.push(this)
+
+	this.addToDom();
+}
+Connection.prototype = {
+	addToDom: function() {
+		// Create the DOM elements needed to draw this connection
+		if(this.type == "wired" || this.type == "wireless") {
+			console.log("Creating connector");
+			//this.el = $("<line x1='0' y1='0' x2='100' y2='100' class='connector' />");
+			var svg = $("#svg_container").svg('get');
+			var x1 = this.a.anchor.left;
+			var y1 = this.a.anchor.top;
+			var x2 = this.b.anchor.left;
+			var y2 = this.b.anchor.top;
+			this.el = $(svg.line(x1, y1, x2, y2));
+			this.el.addClass("connector");
+
+			if(this.type == "wireless") this.el.addClass("wireless");
+		}
+	},
+	update: function() {
+		// Update enpoints
+		var x1 = this.a.anchor.left;
+		var y1 = this.a.anchor.top;
+		var x2 = this.b.anchor.left;
+		var y2 = this.b.anchor.top;
+
+		this.el.attr("x1", x1);
+		this.el.attr("y1", y1);
+		this.el.attr("x2", x2);
+		this.el.attr("y2", y2);
+
+		if(this.a.el.is(":visible") && this.b.el.is(":visible")) {
+			this.el.show();
+		}
+		else {
+			this.el.hide();
+		}
+	}
+
+}
---
+++ device.js
@@ -0,0 +1,57 @@
+// Device prototype
+// ----------------
+function Device(name, type) {
+	this.name = name;
+	this.type = type;
+	this.size = {width: 200, height: 200}
+	this.id = Device.count++;
+
+	this.connections = [];
+
+	this.anchor = {
+		"left": ($(document).width() / 2 - this.size.width / 2),
+		"top": ($(document).height() / 2 - this.size.height / 2) 
+	}
+
+	this.addToDom();
+}
+Device.count = 0;
+Device.prototype = {
+	addToDom: function() {
+		// Create an element and add it to the DOM
+		this.el = $("<div><div class='icon'></div><div class='name'>" + this.name + "</div></div>");
+		this.el.addClass("device " + this.type + " invisible") // Start hidden
+		this.el.attr("id", "device_" + this.id);
+		this.el.offset(this.anchor);	
+		$("#container").append(this.el);
+	},
+	moveTo: function(pos) {
+
+	},
+	show: function() {
+		this.el.fadeIn(500);
+	},
+	hide: function() {
+		this.el.fadeOut(500);
+	},
+	update: function() {
+		if(this.el) {
+			// Update anchor position based on actual icon position
+
+			var e = this.el.find(".icon");
+
+			this.anchor = {left: e.offset().left + e.width() / 2,
+						   top: e.offset().top + e.height() / 2};
+
+		}
+
+		for(var i=0; i<this.connections.length; i++) {
+			this.connections[i].update();
+		}
+	},
+	distanceTo: function(b) {
+		var dx = b.anchor.left - this.anchor.left;
+		var dy = b.anchor.top - this.anchor.top;
+		return Math.sqrt(dx*dx + dy*dy);
+	}
+}
