f59ef8d4d3a3573c63ea894596566d85bd603451
Sam Kronick <sam.kronick@gmail.com>
2013-09-10 14:28:23 -0500

    Added layout.js

---
+++ layout.js
@@ -0,0 +1,186 @@
+function layoutDevices(type) {
+	// Fade everything out
+	$.each(devices, function(index, device) {
+		device.el.stop();
+		//device.hide();
+		device.update();
+	});
+	switch (type) {
+		case "random":
+			$.each(devices, function(index, device) {
+				device.el.fadeIn(500).animate({
+					top: $(document).height()/2 - device.size.height / 2 + random(-500, 500),
+					left: $(document).width()/2 - device.size.width / 2 + random(-500, 500),
+				}, {
+					step: function(n) {
+						device.update();
+					}
+				});
+			});		
+			break;
+		case "random grid":
+			// Place the devices randomly within a grid layout
+			// -----------------------------------------------
+			var grid_width = 6;
+			var grid_height = 4;
+			var grid_interval = 200;
+
+			// Create grid structure
+			var grid = [];
+			for(var i=0; i<grid_height; i++) {
+				grid[i] = [];
+				for(var j=0; j<grid_width; j++) {
+					grid[i][j] = null; // Empty cell
+				}
+			}
+
+			$.each(devices, function(index, device) {
+				var target_i = -1;
+				var target_j = -1;
+
+				var found_space = false;
+				while(!found_space) {
+					target_i = random(0, grid_height);
+					target_j = random(0, grid_width);					
+					if(grid[target_i][target_j] == null) found_space = true;
+				}
+
+				grid[target_i][target_j] = device;
+
+				var x = (target_j - grid_width/2 + 1/2) * grid_interval;
+				var y = (target_i - grid_height/2 + 1/2) * grid_interval;
+
+				device.el.fadeIn(500).animate({
+					top: $(document).height()/2 - device.size.height / 2 + y,
+					left: $(document).width()/2 - device.size.width / 2 + x,
+				}, {
+					step: function(n) {
+						device.update();
+					}
+				});
+			});
+
+			break;
+		case "tree":
+			// Place root node
+			devices[0].el.fadeIn(500).animate({
+				top: 0,
+				left: $(document).width() / 2 - devices[0].size.width / 2
+			}, {
+				step: function(n) {
+					devices[0].update();
+				}
+			});
+
+			treePlace(devices[0], $(document).width() / 2 - devices[0].size.width / 2, devices[0].size.height);
+
+			break;
+
+		case "physics":
+			// Tiny physics simulator
+			$.each(devices, function(i, dev) {
+				dev.el.fadeIn({duration: 1000, queue: false}).animate({
+					left: "+=" + random(-10,10),
+					top: "+=" + random(-10,10)
+				}, 10);
+			});
+
+			devices[0].el.animate({
+				top: $(window).height()/2-devices[0].size.height/2,
+				left: $(window).width()/2-devices[0].size.width/2
+			}, function() {
+				runTinyPhysics();
+			});
+			break;
+	}
+}
+
+function treePlace(root, start_x, start_y) {
+	// Used to recursively place nodes in a tree
+	var children = [];
+
+	// Get connections that lead to children
+	for(var i=0; i<root.connections.length; i++) {
+		if(root.connections[i].a == root) {
+			// root is the routing device
+			children.push(root.connections[i].b);
+		}
+	}
+
+	// Put each child in its place
+	$.each(children, function(index, device) {
+		var x = (index + 1/2 - children.length / 2) * root.size.width;
+		device.el.fadeIn(500).animate({
+			top: start_y,
+			left: start_x + x
+		}, {
+			step: function(n) {
+				device.update();
+			}
+		});
+		// Call treePlace on each child
+		treePlace(device, start_x + x, start_y + root.size.height);
+
+	});
+}
+
+function runTinyPhysics() {
+	// Set target to current position
+	$.each(devices, function(index, device) {
+		device.target = [device.el.offset().left, device.el.offset().top];
+
+	});
+	for(var step = 0; step<500; step++) {
+		$.each(devices, function(index, device) {
+			var F = [0,0];	// force summation
+			// Calculate spring force on each device
+			$.each(device.connections, function(idx, connection) {
+				var sign = device == connection.a ? 1 : -1;
+				var m = (connection.getPhysicsLength() - connection.idealLength) * connection.strength * sign;
+				var v = connection.getPhysicsUnitVector();
+				F[0] += v[0] * m * 0.1;
+				F[1] += v[1] * m * 0.1;
+			});
+
+			// Calculate repulsive force on each device (SLOW AND STUPID WAY I KNOW)
+			$.each(devices, function(idx, other) {
+				if(other == device) return; // Don't repell self
+				if(other.physicsDistanceToSquared(device) < 1000000) {
+					var v = other.physicsVectorTo(device);
+					var l = Math.max(other.physicsDistanceTo(device), 0.001);
+					var m = 1/l;
+					F[0] += v[0]* m * 2;
+					F[1] += v[1]* m * 2;
+				}
+			});
+
+			// Calculate force to keep device on screen
+			if(device.target[0] + device.size.width > $(window).width()) 	 F[0] -= 10;
+			if(device.target[1] + device.size.height > $(window).height()) 	 F[1] -= 10;
+			if(device.target[0] < 0)					 					 F[0] += 10;
+			if(device.target[1] < 1)					 					 F[1] += 10;
+
+
+			// Add (force/mass = accelration) to velocity
+			device.velocity[0] += F[0] / device.mass;
+			device.velocity[1] += F[1] / device.mass;
+
+			// Add damping to velocity
+			device.velocity[0] *= 0.5;
+			device.velocity[1] *= 0.5;
+			
+			device.target[0] += device.velocity[0];
+			device.target[1] += device.velocity[1];
+		});
+	}
+
+	$.each(devices, function(index, device) {
+		device.el.animate({
+			left: device.target[0],
+			top: device.target[1],
+		}, {
+			step: function(n) {
+				device.update();
+			}})
+	});
+}
